750d3bb9bf9b68ed2c6375fe8aa72cb47dd78a7c
Oscar Olivella <oscar.olivella@scrm.eu>
2022-06-05 22:54:42 +0200

    added SnackBarComponent

--- app/src/main/java/com/mrskar/composeworkshop/MainActivity.kt
+++ app/src/main/java/com/mrskar/composeworkshop/MainActivity.kt
@@ -11,8 +11,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.SnackbarResult
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -20,9 +24,11 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mrskar.composeworkshop.composables.Counter
-import com.mrskar.composeworkshop.composables.CustomTopAppBar
 import com.mrskar.composeworkshop.composables.DefaultTopAppBar
 import com.mrskar.composeworkshop.ui.theme.ComposeWorkshopTheme
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.receiveAsFlow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,12 +44,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun Content() {
     val counterState = remember { mutableStateOf(0) }
+    val snackbarHostState = remember { SnackbarHostState() }
+    // we allow only one snackbar to be in the queue here, hence conflated
+    val channel = remember { Channel<Int>(Channel.Factory.CONFLATED) }
+
+    SnackBarComponent(channel, snackbarHostState)
     Scaffold(
+        scaffoldState = rememberScaffoldState(snackbarHostState = snackbarHostState),
         modifier = Modifier.fillMaxSize(),
         backgroundColor = MaterialTheme.colors.background,
         topBar = {
             DefaultTopAppBar()
-            //CustomTopAppBar()
         }
     ) {
         Column(
@@ -51,6 +62,9 @@ private fun Content() {
         ) {
             Counter(counterState.value) { newCount ->
                 counterState.value = newCount
+                if (counterState.value >= 6) {
+                    channel.trySend(0)
+                }
             }
             Divider(
                 modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)
@@ -60,6 +74,36 @@ private fun Content() {
     }
 }
 
+@Composable
+private fun SnackBarComponent(channel: Channel<Int>, snackbarHostState: SnackbarHostState) {
+    LaunchedEffect(key1 = channel) {
+        channel.receiveAsFlow().collect {
+            val result = snackbarHostState.showSnackbar(
+                message = "This is a Snackbar!!",
+                actionLabel = "Take Action!"
+            )
+            when (result) {
+                SnackbarResult.ActionPerformed -> {
+                    snackbarHostState.showSnackbar(
+                        message = "Action performed!!"
+                    )
+                }
+                SnackbarResult.Dismissed -> {
+                    snackbarHostState.showSnackbar(
+                        message = "Action dismissed!!"
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    device = Devices.PIXEL_3,
+    uiMode = UI_MODE_NIGHT_YES
+)
 @Preview(
     showBackground = true,
     showSystemUi = true,
